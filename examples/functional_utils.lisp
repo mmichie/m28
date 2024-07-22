07ac5d5dc9fa7b77248ddff4dc32caebd69dd3ea
Matt Michie <mmichie@gmail.com>
2024-07-21 18:42:52 -0700

    feat(functional_utils): add flatten function and update tests
    - Implement flatten function to handle nested lists
    - Update map, filter, and reduce tests to use flatten
    - Replace #'+ with lambda functions for consistency

--- examples/functional_utils.lisp
+++ examples/functional_utils.lisp
@@ -20,22 +20,29 @@
               (funcall func init (car lst))
               (cdr lst))))
 
+; Helper function to flatten a list
+(defun flatten (lst)
+  (cond
+    ((null? lst) '())
+    ((not (consp lst)) (list lst))
+    (t (append (flatten (car lst)) (flatten (cdr lst))))))
+
 ; Test map
 (print "Mapping (lambda (x) (* x 2)) over (1 2 3 4 5):")
-(print (map (lambda (x) (* x 2)) '(1 2 3 4 5)))
+(print (flatten (map (lambda (x) (* x 2)) '(1 2 3 4 5))))
 
 ; Test filter
 (print "Filtering even numbers from (1 2 3 4 5 6 7 8 9 10):")
-(print (filter (lambda (x) (= (mod x 2) 0)) '(1 2 3 4 5 6 7 8 9 10)))
+(print (flatten (filter (lambda (x) (= (mod x 2) 0)) '(1 2 3 4 5 6 7 8 9 10))))
 
 ; Test reduce
 (print "Sum of (1 2 3 4 5) using reduce:")
-(print (reduce #'+ 0 '(1 2 3 4 5)))
+(print (reduce (lambda (x y) (+ x y)) 0 '(1 2 3 4 5)))
 
 ; Combine all three
 (print "Sum of squares of even numbers in (1 2 3 4 5 6 7 8 9 10):")
-(print (reduce #'+
+(print (reduce (lambda (x y) (+ x y))
                0
-               (map (lambda (x) (* x x))
-                    (filter (lambda (x) (= (mod x 2) 0))
-                            '(1 2 3 4 5 6 7 8 9 10)))))
+               (flatten (map (lambda (x) (* x x))
+                             (flatten (filter (lambda (x) (= (mod x 2) 0))
+                                              '(1 2 3 4 5 6 7 8 9 10)))))))
