33643be7e7d963d361a746189cbcc7bad0e208c3
Matt Michie <mmichie@gmail.com>
2024-07-21 21:44:50 -0700

    feat(list_ops): add new list manipulation functions
    - Implement filter, reduce, reverse, and find functions
    - Add examples demonstrating usage of new functions
    - Improve code formatting and readability

--- examples/list_ops.lisp
+++ examples/list_ops.lisp
@@ -1,4 +1,5 @@
 (defvar my-list '(1 2 3 4 5))
+
 (print "Original list:")
 (print my-list)
 
@@ -15,8 +16,53 @@
       '()
       (cons (func (car lst)) (map func (cdr lst)))))
 
-(defun square (x) 
+(defun square (x)
   (* x x))
 
 (print "Squared list:")
 (print (map square my-list))
+
+(defun filter (pred lst)
+  (if (null? lst)
+      '()
+      (if (pred (car lst))
+          (cons (car lst) (filter pred (cdr lst)))
+          (filter pred (cdr lst)))))
+
+(defun is-even (x)
+  (= (mod x 2) 0))
+
+(print "Filtered even numbers:")
+(print (filter is-even my-list))
+
+(defun reduce (func lst init)
+  (if (null? lst)
+      init
+      (reduce func (cdr lst) (func (car lst) init))))
+
+(defun add (x y)
+  (+ x y))
+
+(print "Sum of list elements using reduce:")
+(print (reduce add my-list 0))
+
+(defun reverse (lst)
+  (defun reverse-iter (lst acc)
+    (if (null? lst)
+        acc
+        (reverse-iter (cdr lst) (cons (car lst) acc))))
+  (reverse-iter lst '()))
+
+(print "Reversed list:")
+(print (reverse my-list))
+
+(defun find (pred lst)
+  (if (null? lst)
+      nil
+      (if (pred (car lst))
+          (car lst)
+          (find pred (cdr lst)))))
+
+(print "First even number in list:")
+(print (find is-even my-list))
+
